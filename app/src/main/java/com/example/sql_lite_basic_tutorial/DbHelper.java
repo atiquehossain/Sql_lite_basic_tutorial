73849117a788afc0b48bdd57a0ec6fa3e2afb348
Atique <atique.hossain@savethechildren.org>
2021-07-31 23:35:39 +0600

    Initial and sql error

---
+++ app/src/main/java/com/example/sql_lite_basic_tutorial/DbHelper.java
@@ -0,0 +1,38 @@
+package com.example.sql_lite_basic_tutorial;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DbHelper extends SQLiteOpenHelper {
+    private static final String TAG ="DatabaseHelper";
+    private static final String TABLE_NAME ="people table";
+    private static final String COL1 ="ID";
+    private static final String COL2 ="name";
+
+
+    public DbHelper(Context context) {
+        super(context,TABLE_NAME,null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+
+
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL2 +" TEXT)";
+        sqLiteDatabase.execSQL(createTable);
+
+    }
+
+    @Override
+
+
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP IF TABLE EXISTS " + TABLE_NAME);
+
+    }
+}
---
+++ app/src/main/java/com/example/sql_lite_basic_tutorial/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.sql_lite_basic_tutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void save(View view) {
+    }
+
+
+    public void view(View view) {
+    }
+}
